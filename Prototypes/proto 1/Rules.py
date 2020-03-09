de9db06fdbaa2c288ef34e650996f736bf82af8d
Virgil Surin <Virgil.surin@student.umons.ac.be>
2020-03-09 14:45:57 +0100

    nothing much

--- Prototypes/proto 1/Rules.py
+++ Prototypes/proto 1/Rules.py
@@ -1,9 +1,6 @@
 from World import *
 from engine import *
 
-def findAPath():
-    pass
-
 def canMove(PawnController, direction, coordinates = None):
     '''
     Boolean, return true if can move
@@ -14,4 +11,17 @@ def canMove(PawnController, direction, coordinates = None):
     if PawnController.board.playerCellList[y][x].wallTo(direction):
         return False
     return True
+
+def findAPath(board, pawnCoord, objective):
+    """
+    board : the board in wich the algorithm will try to find a path
+    pawnCoord : the starting point of the algorithm 
+    objective : the targeted line. Basically the first line or the last line of the board depending
+    on the player
+    """
+    currentCoord = pawnCoord
+    while currentCoord[0] != objective : #currentCoord[0] is the line
+        pass
+
+
 
