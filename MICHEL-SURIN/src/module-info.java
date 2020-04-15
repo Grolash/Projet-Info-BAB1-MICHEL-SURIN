8ad613407baf8058b632ac902cf613f64fc5f372
unknown <Simon.MICHEL@student.umons.ac.be>
2020-04-15 13:09:25 +0200

    added GUI directory

---
+++ MICHEL-SURIN/src/module-info.java
@@ -0,0 +1,7 @@
+module MICHEL.SURIN.main {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens main;
+    opens test;
+}
---
+++ Prototypes/Tests_Projet/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module Tests.Projet.main {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens tests;
+}
