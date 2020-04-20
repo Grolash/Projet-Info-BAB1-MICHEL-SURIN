4157763717438ae4c53acb8e21e5273721b6624a
Virgil Surin <virgil.surin@student.umons.ac.be>
2020-04-20 18:55:26 +0200

    GameUI - board and pawn drawn + mouse click coord

--- MICHEL-SURIN/src/main/java/be/ac/umons/michelsurin/gui/GameUI.java
+++ MICHEL-SURIN/src/main/java/be/ac/umons/michelsurin/gui/GameUI.java
@@ -1,6 +1,9 @@
 package be.ac.umons.michelsurin.gui;
 
 import be.ac.umons.michelsurin.engine.Game;
+import javafx.animation.KeyFrame;
+import javafx.animation.PauseTransition;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -13,15 +16,22 @@ import javafx.scene.image.Image;
 import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 import be.ac.umons.michelsurin.tools.Coord;
 import be.ac.umons.michelsurin.world.Board;
+import javafx.util.Duration;
 
 import java.awt.*;
 
 
 public class GameUI extends Application {
 
+    private Image cellImg = new Image("tile.png");
+    private Image playerPawnImg = new Image("pawn.png");
+    private Image AIPawnImg = new Image("agent.png");
+    public static final int HSpace = 34;
+    public static final int Vspace = 50;
 
 
     public static void main(String[] args) {
@@ -30,7 +40,7 @@ public class GameUI extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        String[] type = {"Human", "Human"};
+        String[] type = {"Human", "Debilus"};
         Game game = new Game(9, type, 10);
 
         primaryStage.setTitle("Quoridor - by Virgil Surin & Simon Michel");
@@ -38,47 +48,71 @@ public class GameUI extends Application {
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
 
-        Canvas canvas = new Canvas(1000,500);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        CellUI[][] cellArray = new CellUI[game.getBoard().getSize()][game.getBoard().getSize()];
+
+        //canvas + GC
+        Canvas cellCanvas = new Canvas(500,500);
+        cellCanvas.autosize();
+        GraphicsContext cellGC = cellCanvas.getGraphicsContext2D();
+
+        Canvas pawnCanvas = new Canvas(500,500);
+        GraphicsContext pawnGC = pawnCanvas.getGraphicsContext2D();
+
+        //board drawing
         for (int i=0; i<game.getBoard().getSize(); i++) {
-            for (int j=0; j< game.getBoard().getSize(); j++) {
-                if (game.getBoard().getCell(getCoordFromSprite(i%34, j%34)).hasPawn()) {
-                    gc.drawImage(new Image("pawn.png"), i*34, j*34);
-                } else {
-                    cellArray[i][j] = new CellUI(new Image("tile.png"), new Coord(i,j));
-                    gc.drawImage(cellArray[i][j].getImg(), j * 34, i * 34);
-                }
+            for (int j=0; j<game.getBoard().getSize(); j++){
+                cellGC.drawImage(cellImg, j*HSpace, i*Vspace);
             }
         }
+        //TODO code from https://stackoverflow.com/questions/49881109/how-to-properly-execute-thread-sleep-in-javafx
+        //this code call the update method with a delay.
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(1000), new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                update(pawnCanvas, pawnGC, game);
+            }
+        }));
+        timeline.play();
 
-        PawnUI pawnUI = new PawnUI(new Image("pawn.png"), game.getBoard().getPawnCoord()[0]);
-        gc.drawImage(pawnUI.getImg(), pawnUI.getCoord().getX()*34, pawnUI.getCoord().getY()*34);
-
-        game.getPlayerArray()[0].move(Game.directions.get("DOWN"));
-        update(gc, game);
-        canvas.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+        //mouse action handling
+        scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                gc.clearRect(event.getX() -2, event.getY() -2, 5,5);
+                Coord eventCoord = getCoordFromPos(event.getX(), event.getY());
+                System.out.println(" x : " + eventCoord.getX() + " | " + "y : " + eventCoord.getY());
             }
         });
 
-        root.getChildren().add(canvas);
+
+        root.getChildren().addAll(cellCanvas, pawnCanvas);
         primaryStage.show();
     }
 
-    public void update(GraphicsContext gc, Game game) {
+    public void update(Canvas canvas, GraphicsContext gc, Game game) {
         //take the game state
-        PawnUI pawnUI = new PawnUI(new Image("pawn.png"), game.getBoard().getPawnCoord()[0]);
-        gc.drawImage(pawnUI.getImg(), pawnUI.getCoord().getX()*34, pawnUI.getCoord().getY()*34 );
+        Board board = game.getBoard();
+        gc.clearRect(0,0, canvas.getHeight(), canvas.getWidth());
+        for (int i=0; i<game.getPlayerArray().length; i++) {
+            Coord coord = game.getPlayerArray()[i].getDependency().getCoord();
+            if (game.getPlayerArray()[i].getType() == "Human") {
+                gc.drawImage(playerPawnImg, coord.getX()*HSpace, coord.getY()*Vspace);
+            } else {
+                gc.drawImage(AIPawnImg, coord.getX()*HSpace, coord.getY()*Vspace);
+            }
+        }
     }
 
-
-    public Coord getCoordFromSprite(int y, int x) {
-        int new_y = y / 34;
-        int new_x = x / 34;
-        return new Coord(new_y, new_x);
+    /**
+     * Given a position in the canvas, will return the coordinates of the sprite the position is pointing to.
+     * It's used to make a transition from the GUI to the game logic system.
+     * @param x
+     * @param y
+     * @return a coordinates instance for a Game object to use.
+     */
+    public static Coord getCoordFromPos(double x, double y) {
+        int game_y = (int) y / Vspace;
+        int game_x = (int) x / HSpace;
+        return new Coord(game_y,game_x);
     }
+
 }
