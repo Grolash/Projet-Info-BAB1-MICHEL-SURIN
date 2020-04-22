1b20f278481fb59040b24f76928826a4e3480684
unknown <Simon.MICHEL@student.umons.ac.be>
2020-04-22 11:24:08 +0200

    Menu interfaced

--- MICHEL-SURIN/src/main/java/be/ac/umons/michelsurin/gui/Menu.java
+++ MICHEL-SURIN/src/main/java/be/ac/umons/michelsurin/gui/Menu.java
@@ -1,10 +1,13 @@
 package be.ac.umons.michelsurin.gui;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Menu extends Application {
@@ -13,11 +16,19 @@ public class Menu extends Application {
     private Button launchButton;
     private Button closeButton;
     private Stage window;
+    private VBox layout;
 
     private ChoiceBox<String> playerNumber;
     private ChoiceBox<String> firstAIDifficultyMenu;
     private ChoiceBox<String> secondAIDifficultyMenu;
     private ChoiceBox<String> thirdAIDifficultyMenu;
+    private ChoiceBox<String> fourthAIDifficultyMenu;
+
+    private int playerNumberInt;
+    private String firstPlayerType;
+    private String secondPlayerType;
+    private String thirdPlayerType;
+    private String fourthPlayerType;
 
     /**
      *
@@ -47,56 +58,69 @@ public class Menu extends Application {
         window.setMinWidth(600);
         window.setMinHeight(600);
 
+        layout = new VBox();
+        layout.setAlignment(Pos.CENTER);
+        layout.setSpacing(20);
+
+
         launchButton = new Button("Launch game!");
         launchButton.setOnAction(e -> launchGame());
+        layout.getChildren().add(launchButton);
+
+        Separator separator = new Separator();
+        layout.getChildren().add(separator);
 
         Label settings = new Label("Settings:");
+        layout.getChildren().add(settings);
 
         //Following lines will set difficulty menus for AI. Listeners will follow in real time selection changes.
         // TODO link listeners values to game values replace sout in lambda function.
         firstAIDifficultyMenu = new ChoiceBox<>();
-        firstAIDifficultyMenu.getItems().addAll("Random AI", "Easy", "Harder");
-        firstAIDifficultyMenu.setValue("Random AI"); //set a default value.
-        firstAIDifficultyMenu.getSelectionModel().selectedItemProperty().addListener( (v, oldValue, newValue) -> System.out.println(newValue));
+        firstAIDifficultyMenu.getItems().addAll("Human", "Random AI", "Easy", "Harder");
+        firstAIDifficultyMenu.setValue("Human"); //set a default value.
+        firstAIDifficultyMenu.getSelectionModel().selectedItemProperty().addListener( (v, oldValue, newValue) -> {
+            firstPlayerType = getPlayerType(newValue);
+        });
 
         secondAIDifficultyMenu = new ChoiceBox<>();
-        secondAIDifficultyMenu.getItems().addAll("Random AI", "Easy", "Harder");
+        secondAIDifficultyMenu.getItems().addAll("Human", "Random AI", "Easy", "Harder");
         secondAIDifficultyMenu.setValue("Random AI"); //set a default value.
-        secondAIDifficultyMenu.getSelectionModel().selectedItemProperty().addListener( (v, oldValue, newValue) -> System.out.println(newValue));
+        secondAIDifficultyMenu.getSelectionModel().selectedItemProperty().addListener( (v, oldValue, newValue) -> {
+            secondPlayerType = getPlayerType(newValue);
+        });
 
         thirdAIDifficultyMenu = new ChoiceBox<>();
-        thirdAIDifficultyMenu.getItems().addAll("Random AI", "Easy", "Harder");
-        thirdAIDifficultyMenu.setValue("Random AI"); //set a default value.
-        thirdAIDifficultyMenu.getSelectionModel().selectedItemProperty().addListener( (v, oldValue, newValue) -> System.out.println(newValue));
+        thirdAIDifficultyMenu.getItems().addAll("Human", "Random AI", "Easy", "Harder");
+        thirdAIDifficultyMenu.setValue("Human"); //set a default value.
+        thirdAIDifficultyMenu.getSelectionModel().selectedItemProperty().addListener( (v, oldValue, newValue) -> {
+            thirdPlayerType = getPlayerType(newValue);
+        });
+
+        fourthAIDifficultyMenu = new ChoiceBox<>();
+        fourthAIDifficultyMenu.getItems().addAll("Human", "Random AI", "Easy", "Harder");
+        fourthAIDifficultyMenu.setValue("Random AI"); //set a default value.
+        fourthAIDifficultyMenu.getSelectionModel().selectedItemProperty().addListener( (v, oldValue, newValue) -> {
+            fourthPlayerType  = getPlayerType(newValue);
+        });
 
         //Player number menus, will define which difficulty options are enabled.
         playerNumber = new ChoiceBox<>();
-        playerNumber.getItems().addAll( "2 Players", "2 Players (1 AI)",
-                "4 Players", "4 Players (1 AI)", "4 Players (2 AI)", "4 Players (3 AI)");
-        playerNumber.setValue("2 Players (1 AI)"); //set a default value.
+        playerNumber.getItems().addAll( "2 Players",
+                "4 Players");
+        playerNumber.setValue("2 Players"); //set a default value.
+        layout.getChildren().add(playerNumber);
         playerNumber.getSelectionModel().selectedItemProperty().addListener( (v, oldValue, newValue) -> {
-            if (newValue.equals("2 Players (1 AI)")){
+            if (newValue.equals("2 Players")){
+                playerNumberInt = 2;
                 Label difficulty = new Label("AI difficulty:");
-                // TODO set firstAIDifficultyMenu to layout
+                layout.getChildren().addAll(firstAIDifficultyMenu, secondAIDifficultyMenu);
             }
-            else if(newValue.equals("4 Players (1 AI)")){
+            if (newValue.equals("4 Players")){
+                playerNumberInt = 4;
                 Label difficulty = new Label("AI difficulty:");
-                // TODO set firstAIDifficultyMenu to layout
+                layout.getChildren().addAll(firstAIDifficultyMenu, secondAIDifficultyMenu, thirdAIDifficultyMenu, fourthAIDifficultyMenu);
             }
-            else if (newValue.equals("4 Players (2 AI)")){
-                Label firstDifficulty = new Label("First AI difficulty:");
-                Label secondDifficulty = new Label("Second AI difficulty:");
-                // TODO set both difficulty menus to layout
-            }
-            else if (newValue.equals("4 Players (3 AI)")){
-                Label firstDifficulty = new Label("First AI difficulty:");
-                Label secondDifficulty = new Label("Second AI difficulty:");
-                Label thirdDifficulty = new Label("Third AI difficulty:");
-                // TODO set all difficulty menus to layout
-            }
-            // TODO set human players
         });
-
 
 
 
@@ -111,6 +135,44 @@ public class Menu extends Application {
         if (answer) {
             window.close();
         }
+    }
+
+    private String getPlayerType(String string) throws IllegalArgumentException{
+        if (string.equals("Human")){
+            return string;
+        }
+        else if(string.equals("Random AI")){
+            return "Debilus";
+        }
+        else if(string.equals("Easy")){
+            return "Smarted";
+        }
+        else if(string.equals("Harder")){
+            return "Smart";
+        }
+        else {
+            throw new IllegalArgumentException("Wrong player type!");
+        }
+    }
+
+    public int getPlayerNumberInt() {
+        return playerNumberInt;
+    }
+
+    public String getFirstPlayerType() {
+        return firstPlayerType;
+    }
+
+    public String getSecondPlayerType() {
+        return secondPlayerType;
+    }
+
+    public String getThirdPlayerType() {
+        return thirdPlayerType;
+    }
+
+    public String getFourthPlayerType() {
+        return fourthPlayerType;
     }
 
     private void launchGame(){
